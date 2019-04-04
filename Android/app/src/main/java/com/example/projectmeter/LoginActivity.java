1807098080c4e28c44067c47aa4d7ca63ed838e5
harshendrashah <harshendrashah812@gmail.com>
2019-04-05 00:55:49 +0530

    Updated Login Layout

--- Android/app/src/main/java/com/example/projectmeter/LoginActivity.java
+++ Android/app/src/main/java/com/example/projectmeter/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,63 +12,32 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText inputEmail, inputPassword;
+    private EditText userName;
     private ProgressBar progressBar;
-    private Button btnSignup, btnLogin, btnReset;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        inputEmail = findViewById(R.id.email);
-        inputPassword = findViewById(R.id.password);
-        progressBar = findViewById(R.id.progressBar);
-        btnSignup = findViewById(R.id.btn_signup);
+        userName = findViewById(R.id.userName);
         btnLogin = findViewById(R.id.btn_login);
-        btnReset = findViewById(R.id.btn_reset_password);
-
-        btnSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
-            }
-        });
-
-        btnReset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, ResetPasswordActivity.class));
-            }
-        });
-
+        progressBar = findViewById(R.id.progressBar);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = inputEmail.getText().toString();
-                final String password = inputPassword.getText().toString();
-
-                if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                if (TextUtils.isEmpty(password)) {
+                String name = userName.getText().toString();
+                if (TextUtils.isEmpty(name)) {
                     Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
                     return;
+                } else {
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    i.putExtra("username", name);
+                    startActivity(i);
                 }
 
                 progressBar.setVisibility(View.VISIBLE);
-
-                if(password.equals("test123")) {
-                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                    Log.i("8888", email);
-                    i.putExtra("emailid", email);
-                    startActivity(i);
-
-                }
-                else
-                    Toast.makeText(LoginActivity.this, "Incorrect Username / Password", Toast.LENGTH_SHORT).show();
             }
         });
     }
