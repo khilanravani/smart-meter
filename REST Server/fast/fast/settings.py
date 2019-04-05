687132c5708a301517a6353da6d941e96e81dd19
hsankesara <heetsankesara3@gmail.com>
2019-04-05 14:57:08 +0530

    Deployment stuff

--- REST Server/fast/fast/settings.py
+++ REST Server/fast/fast/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 """
 Django settings for fast project.
 
@@ -79,10 +80,10 @@ WSGI_APPLICATION = 'fast.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(
+            os.path.join(BASE_DIR, 'db.sqlite3'))
+    )
 }
 
 
