7571f45941dd8347784db0bc2bd826149ea31a92
hsankesara <heetsankesara3@gmail.com>
2019-04-06 01:43:40 +0530

    dev stuff

--- REST Server/fast/fast/settings.py
+++ REST Server/fast/fast/settings.py
@@ -62,7 +62,14 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'fast.urls'
+
 CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = (
+    'smart-meter-guj.herokuapp.com',
+    '127.0.0.1'
+)
+
 CORS_ALLOW_METHODS = (
     'GET',
     'POST',
