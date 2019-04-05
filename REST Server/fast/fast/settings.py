1a6d76b8008e00515091c79090b81ff3b4b2ed25
hsankesara <heetsankesara3@gmail.com>
2019-04-06 00:20:12 +0530

    deployment stuff

--- REST Server/fast/fast/settings.py
+++ REST Server/fast/fast/settings.py
@@ -1,4 +1,6 @@
 import dj_database_url
+import django_heroku
+
 """
 Django settings for fast project.
 
@@ -129,3 +131,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+django_heroku.settings(locals())
