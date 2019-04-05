8a5fbcdc1cf93439a036b057c595a02ea906f916
harshendrashah <harshendrashah812@gmail.com>
2019-04-06 02:39:08 +0530

    Updates

--- WebApp/src/components/UserDetails.js
+++ WebApp/src/components/UserDetails.js
@@ -2,8 +2,7 @@ import React, {Component} from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Label } from 'recharts';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
-import axios from 'axios'; 
-import data from '../data/data.json';
+import axios from 'axios';
 
 class UserDetails extends Component {
 
@@ -12,6 +11,7 @@ class UserDetails extends Component {
 		this.state = {
             userdetails: null,
             plot: null,
+            history: null,
             showPlot: false,
         };
         this.onClick = this.onClick.bind(this);
@@ -24,12 +24,19 @@ class UserDetails extends Component {
                 this.setState({
                   userdetails: userdetails
                 })
-            })
+            });
         axios.get('http://smart-meter-guj.herokuapp.com/rest/user/record/' + this.props.match.params.username + '/?format=json')
             .then(res => {
                 const data = res.data;
                 this.setState({
                     plot: data
+                })
+            });
+        axios.get('http://smart-meter-guj.herokuapp.com/rest/user/bill/'+ this.props.match.params.username +'/?format=json')
+            .then(res => {
+                const bills = res.data;
+                this.setState({
+                    history: bills
                 })
             })
     }
@@ -46,14 +53,17 @@ class UserDetails extends Component {
                 <div className='user-details'>
                     <div className="user-info">    
                         <p><span className="bold">Meter ID:</span> {this.state.userdetails.meter_id}</p>
+                        <p><span className="bold">User Name:</span> {this.state.userdetails.user.username}</p>
                         <p><span className="bold">First Name:</span> {this.state.userdetails.user.first_name}</p>
                         <p><span className="bold">Last Name:</span> {this.state.userdetails.user.last_name}</p>
                         <p><span className="bold">Email:</span> {this.state.userdetails.user.email}</p>
                         <p><span className="bold">Phone Number:</span> {this.state.userdetails.contact_number}</p>
-
-                        <div>
+                        <div className="btns-div">
                             <Button variant="contained" color="primary" onClick={this.onClick}>
                             Generate Graph
+                            </Button>
+                            <Button variant="contained" color="secondary">
+                            Block Supply
                             </Button>
                         </div>
                         <Card style={{display: visible ? 'inline-block' : 'none', padding:'20px', marginTop: '40px'}}>
@@ -73,23 +83,24 @@ class UserDetails extends Component {
                     <div>
                         <p><span className="bold">History:</span></p>
                         <Card className="bill">
-                            <div>
-                                {data.data.map((meter) => 
-                                    <div className="bill-details">
-                                        <div className="card">
-                                            <p><span className="bold">Time:</span> {"12:29:30"}</p>
-                                            <p><span className="bold">Amount:</span> {123456}</p>
-                                            <p><span className="bold">Paid?</span> {"Yes!"}</p>
+                            {this.state.history !== null ? (
+                                <div>
+                                    {this.state.history.map((data) => 
+                                        <div className="bill-details">
+                                            <div className="card">
+                                                <p><span className="bold">Time:</span> {data.time}</p>
+                                                <p><span className="bold">Amount:</span> {data.cost}</p>
+                                                <p><span className="bold">Paid?</span> {data.is_paid? "Yes!" : "No!"}</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                    )
-                                }
-                            </div>
+                                        )
+                                    }
+                                </div>
+                            ) : null}
                         </Card>
                     </div>
                 </div>
-                ) : null}
-                      
+                ) : null}      
             </div>
         );
     }
